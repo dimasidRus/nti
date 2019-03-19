e05e803404390a55a85811ff0d67a4c598b5c240
Dima <dimasidlol@gmail.com>
2019-03-19 15:17:04 +1000

    Fixed file structure

---
+++ VR/Assets/Scripts/ConveyorLocomotion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    GameObject[] conveyors;
+
+	void Start () {
+	}
+	
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void GetConveyorTransforms()
+    {
+        conveyors = GameObject.FindGameObjectsWithTag("Conveyor");
+        //conveyors = new Transform[conveyorObjects.Length];
+        //for (int i = 0; i < conveyorObjects.Length; i++)
+        //    conveyors[i] = conveyorObjects[i].transform;
+    }
+
+    //TODO add collision check
+    bool CoveyorValid(Transform conveyor)
+    {
+        bool valid = true;
+        if ()
+        return valid;
+    }
+
+    void LocomotionTick()
+    {
+
+    }
+}
