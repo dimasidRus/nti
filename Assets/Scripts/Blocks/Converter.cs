4fdb1c20981b7f5a692777e9cced9dbe47d25457
Dima <dimasidlol@gmail.com>
2019-03-20 11:11:17 +1000

    Resoursee spawn mechanic and implemented resourse converters

---
+++ Assets/Scripts/Blocks/Converter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Converter : MonoBehaviour
+{
+
+    private BoxCollider trigger;
+    private SpawnCollider spawn;
+
+    public int resourseId1;
+    public int neededAmount1;
+    public int currentAmount1;
+
+    public int resourseId2;
+    public int neededAmount2;
+    public int currentAmount2;
+
+    public int outputId;
+    public int outputBuffer;
+
+    public GameObject resourePrefab;
+
+
+    void Start()
+    {
+        spawn = gameObject.GetComponentInChildren<SpawnCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentAmount1 >= neededAmount1 && currentAmount2 >= neededAmount2)
+        {
+            currentAmount1 -= neededAmount1;
+            currentAmount2 -= neededAmount2;
+            outputBuffer++;
+        }
+        if (spawn.isFree)
+        {
+            Instantiate(resourePrefab, position:spawn.transform.position, rotation:spawn.transform.rotation);
+        }
+    }
+
+    public void RegisterTrigger(Collider other)
+    {
+        GameObject otherGO = other.gameObject;
+        int otherId = otherGO.GetComponent<Resourse>().id;
+
+        if (otherId == resourseId1)
+            currentAmount1++;
+        else if (otherId == resourseId2)
+            currentAmount2++;
+
+        Destroy(otherGO);
+    }
+}
---
+++ Assets/Scripts/BlocksUtils/RegisterResourse.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RegisterResourse : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Movable")
+            GetComponentInParent<Converter>().RegisterTrigger(other);
+    }
+}
