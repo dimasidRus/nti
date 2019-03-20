e3953837f3b657d4d1cd24c0af9d1ee0862e6623
Олимпиада.НТИ <Olimpnti09>
2019-03-20 13:10:11 +1000

    Not working models

---
+++ Assets/Scripts/ConverterLocomotion.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConverterBehaviourScript : MonoBehaviour {
+
+    public float locomotionWidth = 0.1f;
+    public float locomotionHeight = 0.1f;
+
+    GameObject[] converters;
+
+	void Start () {
+	}
+	
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void GetConverterTransforms()
+    {
+        converters = GameObject.FindGameObjectsWithTag("Converter");
+    }
+    
+    bool ConverterValid(GameObject conveyor)
+    {
+        bool valid = true;
+
+        Transform conTransform = conveyor.transform;
+        Vector3 conPos = conTransform.position;
+
+        Vector3 leftBottom;
+        Vector3 rightTop;
+        leftBottom = conPos - conTransform.right * locomotionWidth - conTransform.up * locomotionHeight;
+        rightTop = conPos + conTransform.forward * (1 - locomotionWidth) + conTransform.right * locomotionWidth + conTransform.up * locomotionHeight;
+
+        valid &= transform.position.x >= Mathf.Min(leftBottom.x, rightTop.x);
+        valid &= transform.position.x <= Mathf.Max(leftBottom.x, rightTop.x);
+        valid &= transform.position.y >= Mathf.Min(leftBottom.y, rightTop.y);
+        valid &= transform.position.y <= Mathf.Max(leftBottom.y, rightTop.y);
+        valid &= transform.position.z >= Mathf.Min(leftBottom.z, rightTop.z);
+        valid &= transform.position.z <= Mathf.Max(leftBottom.z, rightTop.z);
+
+        return valid;
+    }
+
+    void LocomotionTick()
+    {
+
+    }
+}
